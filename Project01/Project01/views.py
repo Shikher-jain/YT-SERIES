0eb59adfd46e577d6346869a270b4f4b52cea8f3
Shikher Jain <shikherjain786@gmail.com>
2024-12-24 23:07:40 +0530

    Django

--- Project01/Project01/views.py
+++ Project01/Project01/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, World : Django Project01 Test:home")
+    # return HttpResponse("Hello, World : Django Project01 Test:home")
+    return render(request,"website/index.html")
 
 def about(request):
     return HttpResponse("Hello, World : Django Project01 Test:about")
 
 def contact(request):
     return HttpResponse("Hello, World : Django Project01 Test:contact")
+
